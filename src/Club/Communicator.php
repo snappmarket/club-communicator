cda0332177cb3677f45121ee76a91f23f048d5ac
EmiTis <emitis89@gmail.com>
2021-02-03 21:36:42 +0330

    add security-token in default headers

--- src/Club/Communicator.php
+++ src/Club/Communicator.php
@@ -2,12 +2,30 @@
 
 namespace SnappMarket\Club;
 
+use Psr\Log\LoggerInterface;
 use SnappMarket\Club\Requests\LoyaltyJwtRequest;
 use SnappMarket\Club\Results\LoyaltyJwtResult;
 use SnappMarket\Communicator\Communicator as BasicCommunicator;
 
 class Communicator extends BasicCommunicator
 {
+    const SECURITY_TOKEN_HEADER = 'Service-Security';
+
+
+
+    public function __construct(
+        string $baseUri,
+        string $securityToken,
+        array $headers = [],
+        ?LoggerInterface $logger = null
+    ) {
+        $headers[static::SECURITY_TOKEN_HEADER] = $securityToken;
+
+        parent::__construct($baseUri, $headers, $logger);
+    }
+
+
+
     public function getLoyaltyJwt(LoyaltyJwtRequest $request)
     {
         $uri      = '/api/loyalty/v1/users/' . $request->getUserId() . '/jwt-token';
